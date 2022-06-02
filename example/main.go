5f4881cd089461936c69856389a0b67478fca5ca
liushan <liushan@huishoubao.com.cn>
2022-06-02 19:05:27 +0800

    添加测试用例

--- example/main.go
+++ example/main.go
@@ -4,21 +4,24 @@ import (
 	"context"
 	"fmt"
 	"github.com/hsbteam/rest_client"
+	"github.com/tidwall/gjson"
 	"net/http"
 	"time"
 )
 
+//RestDome1 一个服务一个结构
 type RestDome1 struct {
 	token string
 }
 
+// 一个接口一个常量
 const (
 	ProductDetail = iota
 	ProductAdd    = iota
 )
 
 func (res *RestDome1) ConfigBuilds(_ context.Context) (map[int]rest_client.RestBuild, error) {
-	//动态构建配置发生错误时返回错误即可
+	//分配配个每个接口
 	return map[int]rest_client.RestBuild{
 		ProductDetail: &rest_client.AppRestBuild{
 			HttpMethod: http.MethodGet,
@@ -34,11 +37,14 @@ func (res *RestDome1) ConfigBuilds(_ context.Context) (map[int]rest_client.RestB
 		},
 	}, nil
 }
+
+//ConfigName 配置名称
 func (res *RestDome1) ConfigName(_ context.Context) (string, error) {
 	//动态获取配置发生错误时返回错误即可
 	return "product", nil
 }
 
+//Token 如Oauth等服务会有当前登录TOKEN
 func (res *RestDome1) Token(_ context.Context) (string, error) {
 	//动态获取TOKEN发生错误时返回错误即可
 	return res.token, nil
@@ -46,12 +52,13 @@ func (res *RestDome1) Token(_ context.Context) (string, error) {
 
 func main() {
 	client := rest_client.NewRestClientManager()
-	//配置
+	//配置,其中 Name在 ConfigBuilds 中使用
 	client.SetRestConfig(&rest_client.AppRestConfig{
 		Name:      "product",
 		AppKey:    "hjx",
 		AppSecret: "f4dea3417a2f52ae29a635be00537395",
 		AppUrl:    "http://127.0.0.1:8080",
+		//EventCreate 非必须,如需要得到请求过程 也自行实现 RestEvent
 		EventCreate: func(_ context.Context) rest_client.RestEvent {
 			return rest_client.NewAppRestEvent(
 				func(method string, url string, httpCode int, httpHeader map[string][]string, request []byte, response []byte, err error) {
@@ -64,7 +71,7 @@ func main() {
 				})
 		},
 	})
-	//使用
+	//调用接口
 	data := (<-client.NewApi(&RestDome1{
 		token: "",
 	}).Do(context.Background(), ProductAdd, map[string]string{
@@ -75,15 +82,15 @@ func main() {
 		return
 	}
 	fmt.Printf("data:%s", data.GetData(""))
-	//获取数据并校验
-	//data.GetData(rest_client.JsonKey{
-	//	Path: "data",
-	//	ToType: func(result gjson.Result) interface{} {
-	//		return result.String()
-	//	},
-	//	Tag: "glen:10",
-	//	JsonValid: rest_client.JsonValid{
-	//		Context: context.Background(),
-	//	},
-	//})
+	//调用接口
+	data.GetData(rest_client.JsonKey{
+		Path: "data",
+		ToType: func(result gjson.Result) interface{} {
+			return result.String()
+		},
+		Tag: "glen:10",
+		JsonValid: rest_client.JsonValid{
+			Context: context.Background(),
+		},
+	})
 }
